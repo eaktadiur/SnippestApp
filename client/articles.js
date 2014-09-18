441157d9d3a80d1326e7e24c93ccf9dfcd437b81
tkmou <tkmoubd@gmail.com>
2014-09-18 18:35:20 +0600

    add jquery masonry

--- client/articles.js
+++ client/articles.js
@@ -6,13 +6,23 @@ Template.menu.events({
 	}
 })
 
+Template.article.events({
+	'click .like':function(evt,tmp){
+		var curlike = Likes.findOne({muser:Meteor.userId(),article:tmp.data._id});
+		if(!curlike){
+			Likes.insert({muser:Meteor.userId(),article:tmp.data._id});				
+		} 
+		Session.set('updated',new Date());
+	}
+})
+
 Template.addform.events({
 	'click .save': function(e, tmp){
 		var description = tmp.find('.description').value;
 		var name = tmp.find('.name').value;
 		var url = tmp.find('.url').value;
 		var height = getRandomInt(150,350);
-		Articles.insert({description:description,name:name,src:url,height:height,width:'25%'});
+		Articles.insert({description:description,name:name,src:url,height:height,width:'25%', create_at: new Date()});
 		Session.set('adding_interest',false);
 
 		console.log('Save button click');
--- client/variables.js
+++ client/variables.js
@@ -7,3 +7,33 @@ Template.pinterest.articles = function(){
 	return Articles.find(search, {limit: 10});
 }
 
+Template.pinterest.rendered = function(){
+	setTimeout(function(){
+		masonize(function(){
+		});
+		
+	},1000)
+	$('.search-query input').focus();
+	
+}
+
+Template.article.numlikes = function(){
+	return Likes.find({article:this._id}).count();
+}
+Template.article.likethis = function(){
+	var curlike = Likes.findOne({muser:Meteor.userId(),article:this._id});
+	if(curlike)
+	return "You Like This"
+}
+
+function masonize(callback){
+	var $container = $('#mainContainer');
+	// initialize
+	$container.masonry({
+	  itemSelector: '.item',
+	  gutter:20
+	});
+	var msnry = $container.data('masonry');
+	if(callback){callback()};
+}
+
